ae53ebeefdf01ac1ab245c09abe30fc4a881096e
jordipimoreno <jordi.pi@students.salle.url.edu>
2022-10-20 19:58:57 +0200

    Sign Up style

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,30 +1,53 @@
-<template>
-<html>
-        <head>
-        <title> Sign Up </title>
+<template >
+<html >
     
-    </head>
-    
-        <center> <h1> Create your account: </h1> </center>
-        <form>
-            <div>
-              <label>Username : </label>   
-            <input type="text" placeholder="Enter Username" name="username" required>  
-                       
-            </div>
-
-            <div>
-                <label>Password : </label> 
-                <input type="password" placeholder="Enter Password" name="password" required>
-            </div>
-
-            <div>
-                <label>Confirm Password : </label>
-                <input type="confirm password" placeholder="Confirm Password" name="confirm password" required> 
-            </div>
+        <header class ="header2">
             
+            <h1>Sign Up</h1>
+            
+        </header>
+     
+            
+            
+        <div class ="body2">     
+                <div >
+                <label>Username : </label>   
+                <input type="text" placeholder="Enter Username" name="username" required>  
+                        
+                </div>
+
+                <div>
+                    <label>Password : </label> 
+                    <input type="password" placeholder="Enter Password" name="password" required>
+                </div>
+
+                <div>
+                    <label>Confirm Password :</label>
+                    <input type="confirm password" placeholder="Confirm Password" name="confirm password" required> 
+                </div>
+                <br>
+                <button type = "submit"><strong>Sign Up</strong></button>
+                
+        </div>      
         
-        </form>
+
+            
+
+        
 
 </html>
 </template>
+<style>
+.header2{
+    background-color: turquoise ;
+    font-family: 'Courier New', Courier, monospace;
+    font-size:medium    ;   
+  }
+.body2{
+
+    background-color: turquoise;
+    font-family: Arial, Helvetica, sans-serif;
+    display: flex;
+    flex-direction: column-reverse;
+}
+
