e6feca4be6bf32d8a0d7e82326dbb0b9897fec66
jordipimoreno <jordi.pi@students.salle.url.edu>
2022-10-27 20:03:25 +0200

    Elocuente magnificamente genial, rebotador de anos

--- src/views/CreateEvent.vue
+++ src/views/CreateEvent.vue
@@ -1,11 +1,65 @@
 <template>
     <main>
-        <title>
+        <h1>
             EVENT CREATION!
-        </title>
+        </h1>
 
+        
+        <div class="foto" >
+         
+               <article class="foto2"> Welcome "Creator name" to event creation</article>
+               <article><img class="circular-image" src="https://cdn.pixabay.com/photo/2021/05/04/13/29/portrait-6228705_960_720.jpg" width="100" height="100"/> </article> 
 
+           
+ 
+        </div>
+        
+        <div class="norma">
+            <div >
+                <label>Name </label> 
+                <input class="separacio" type="text" placeholder="" name="" required> 
+            </div>
+            
+            <div>
+                <label>Date </label> 
+                <input type="Date" placeholder="" name="" required>
+
+                <label>Duration </label> 
+                <input type="Time" placeholder="" name="" required> 
+            </div>
+            <div >
+                <label>Location </label> 
+                <input type="Text" placeholder="" name="" required> 
+
+            </div>
+        </div>
+        
 
 
     </main>
 </template>
+
+<style>
+.foto2 {
+    max-width: 180px;
+}
+.foto{
+    display: flex;
+    align-items:center;
+    justify-content: space-around;
+    
+}
+.norma{
+    display: flex;
+     flex-direction: column;
+     align-items: flex-start;
+     justify-content: space-around;
+    
+
+}
+
+.separacio{
+   margin-left: 40px;
+   width: 200px;
+}
+</style>
