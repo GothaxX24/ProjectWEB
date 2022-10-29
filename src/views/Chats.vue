bc3de78321e5143142578a03f3f085c8e6ac4a28
mireia11 <115173193+mireia11@users.noreply.github.com>
2022-10-29 19:14:46 +0200

    chats v2

--- src/views/Chats.vue
+++ src/views/Chats.vue
@@ -15,21 +15,40 @@
             <div class = body_chats2>
                 <img class="circular-image" src="https://2.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=180" width="30" height="30"/>
                 <label>User 1</label> 
+                <div class = body_chats10>
+                    <label>Send message</label>
+                </div>
+                <div class = body_chats11>
+                    <label><bold>You have 1 message!</bold></label>
+                </div>
             </div>
+
     
             <div class = body_chats3>
                 <img class="circular-image" src="https://2.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=180" width="30" height="30"/>
                 <label>User 2</label> 
+                <div class = body_chats10>
+                    <label>Send message</label>
+                </div>
             </div>
             
             <div class = body_chats4>
                 <img class="circular-image" src="https://2.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=180" width="30" height="30"/>
                 <label>User 3</label> 
+                <div class = body_chats10>
+                    <label>Send message</label>
+                </div>
             </div>
            
             <div class = body_chats5>
                 <img class="circular-image" src="https://2.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=180" width="30" height="30"/>
                 <label>User 4</label> 
+                <div class = body_chats10>
+                    <label>Send message</label>
+                </div>
+                <div class = body_chats11>
+                    <label><bold>You have 1 message!</bold></label>
+                </div>
             </div>
 
             <br>
@@ -41,16 +60,25 @@
             <div class = body_chats7>
                 <img class="circular-image" src="https://2.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=180" width="30" height="30"/>
                 <label>User 5</label> 
+                <div class = body_chats10>
+                    <label>Send message</label>
+                </div>
             </div>
             
             <div class = body_chats8>
                 <img class="circular-image" src="https://2.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=180" width="30" height="30"/>
-                <label>User 6</label>    
+                <label>User 6</label>  
+                <div class = body_chats10>
+                    <label>Send message</label>
+                </div>  
             </div>
 
             <div class = body_chats9>
                 <img class="circular-image" src="https://2.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=180" width="30" height="30"/>
-                <label>User 7</label>    
+                <label>User 7</label>  
+                <div class = body_chats10>
+                    <label>Send message</label>
+                </div>  
             </div>
     </div>      
     
@@ -132,6 +160,33 @@ flex-direction: row;
     font-family: Arial, Helvetica, sans-serif;
     display: flex;
     flex-direction: row;
+}
+
+.body_chats10{
+    
+    font-size: "3";
+    color: white;
+    text-shadow: 0 0 3px #010303;
+    background-color: turquoise;
+    font-family: Arial, Helvetica, sans-serif;
+    flex-direction: row;
+    text-align: center;
+    padding: 0 50px 0 55px;
+    margin-top: 3px;
+}
+
+.body_chats11 {
+
+    border:5px solid rgb(255, 255, 255);
+    font-size: "3";
+    color: rgb(0, 0, 0);
+   
+    font-family: Arial, Helvetica, sans-serif;
+    flex-direction: row;
+    text-align: right;
+    padding: 0 50px 0 55px;
+    margin-top: 0px;
+
 }
 </style>
 
