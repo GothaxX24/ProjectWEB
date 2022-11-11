660effe71e74c249c900c2d24e161f6f37da5c28
mireia11 <115173193+mireia11@users.noreply.github.com>
2022-11-11 20:00:05 +0100

    friends,inchat

--- src/views/inChat.vue
+++ src/views/inChat.vue
@@ -1,28 +1,26 @@
 <template>
                 
-                 
+         <div class = body_inchat6>
             <div class = body_inchat1>
                 <img class="circular-image" src="https://2.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=180" width="30" height="30"/>
                 <label><strong>User 1</strong></label>         
             </div>
-    
-            <div class = body_inchat2>
+                
+                <div class = body_inchat2>
 
-                <div class = body_inchat3>
-                    <label><strong>Hey!</strong></label>
-                </div>
-                <div class="prova1">
-                    <div class = body_inchat4>
-                        <label><strong>Hi! How are you?</strong></label>
+                    <div class = body_inchat3>
+                        <label><strong>Hey!</strong></label>
                     </div>
-                </div>
-                <div class = body_inchat5>
-                    <label><strong>Great! I went to the event you recommended.</strong></label>
-                </div>
-                
-                
-                
+                    <div class="prova1">
+                        <div class = body_inchat4>
+                            <label><strong>Hi! How are you?</strong></label>
+                        </div>
+                    </div>
+                    <div class = body_inchat5>
+                        <label><strong>Great! I went to the event you recommended.</strong></label>
+                    </div>  
             </div>
+         </div>       
 
 </template>
 
@@ -32,7 +30,7 @@
     flex-direction: row-reverse;
 }
 .body_inchat1{
-width:70;
+width:150;
 height:50px;
 color: white;
 background:rgb(0, 47, 156);
@@ -93,6 +91,21 @@ border-radius: 5px;
 display: flex;
 justify-content: center;
 flex-direction: row;
+border: 2px solid #dedede;
+background-color: #f1f1f1;
+border-radius: 5px;
+padding: 2px;
+margin: 10px 0;
+text-align: left;
+}
+
+.body_inchat6 {
+
+background:rgb(0, 253, 245);
+border-radius: 5px;
+display: flex;
+align-items: center;
+flex-direction: column;
 border: 2px solid #dedede;
 background-color: #f1f1f1;
 border-radius: 5px;
