9ce07d6465151d4f9a65d32873ad52ce08e23cdc
jordipimoreno <jordi.pi@students.salle.url.edu>
2022-11-12 16:46:54 +0100

    commit

--- src/views/EventsList.vue
+++ src/views/EventsList.vue
@@ -38,58 +38,86 @@
                     <div>
                         <div class="aaa">
                             <label class="marginnameofevent">EventName</label>
-                            <img class="margefotoperfil" src="https://cdn.pixabay.com/photo/2021/05/04/13/29/portrait-6228705_960_720.jpg" width="35" height="35"/>
-                            <div class="marge4">
-                                <label class="nameofcreator">CreatorName</label>
-                            </div>
-                        </div>
-                        <div class="margetop3">
-                            <label class="marginstreet">11/12/2022</label>
-                            <label class="streetsize">c/NameStr 291, BCN</label>
-                        </div>
-                    </div>
-                </div>
-                <div class="margegran">
-                    <img src="https://www.marquid.com/wp-content/uploads/2017/06/6197706_orig.jpg" width="75" height="75 ">
-                    <div>
-                        <div class="aaa">
-                            <label class="marginnameofevent">EventName</label>
-                            <img class="margefotoperfil" src="https://cdn.pixabay.com/photo/2021/05/04/13/29/portrait-6228705_960_720.jpg" width="35" height="35"/>
-                            <div class="marge4">
-                                <label class="nameofcreator">CreatorName</label>
-                            </div>
-                        </div>
-                        <div class="margetop3">
-                            <label class="marginstreet">11/12/2022</label>
-                            <label class="streetsize">c/NameStr 291, BCN</label>
-                        </div>
-                    </div>
-                </div>
-                <div class="margegran">
-                    <img src="https://www.marquid.com/wp-content/uploads/2017/06/6197706_orig.jpg" width="75" height="75 ">
-                    <div>
-                        <div class="aaa">
-                            <label class="marginnameofevent">EventName</label>
-                            <img class="margefotoperfil" src="https://cdn.pixabay.com/photo/2021/05/04/13/29/portrait-6228705_960_720.jpg" width="35" height="35"/>
+                            <img class="margefotoperfil" src="https://cdn.pixabay.com/photo/2021/05/04/13/29/portrait-6228705_960_720.jpg" width="45" height="45"/>
                             <div class="marge4">
                                 <label class="nameofcreator">CreatorName</label>
                                 <form>
-                                    <p class="clasificacion">
-                                        <input id="radio1" type="radio" name="estrellas" value="5"><!--
-                                        --><label for="radio1">★</label><!--
-                                        --><input id="radio2" type="radio" name="estrellas" value="4"><!--
-                                        --><label for="radio2">★</label><!--
-                                        --><input id="radio3" type="radio" name="estrellas" value="3"><!--
-                                        --><label for="radio3">★</label><!--
-                                        --><input id="radio4" type="radio" name="estrellas" value="2"><!--
-                                        --><label for="radio4">★</label><!--
-                                        --><input id="radio5" type="radio" name="estrellas" value="1"><!--
-                                        --><label for="radio5">★</label>
-                                    </p>
+                                    <div class="clasificacion">
+                                        <input id="radio1" type="radio" name="estrellas" value="5">
+                                        <label for="radio1">★</label>
+                                        <input id="radio2" type="radio" name="estrellas" value="4">
+                                        <label for="radio2">★</label>
+                                        <input id="radio3" type="radio" name="estrellas" value="3">
+                                        <label for="radio3">★</label>
+                                        <input id="radio4" type="radio" name="estrellas" value="2">
+                                        <label for="radio4">★</label>
+                                        <input id="radio5" type="radio" name="estrellas" value="1">
+                                        <label for="radio5">★</label>
+                                    </div>
                                 </form>
                             </div>
                         </div>
-                        <div class="margetop3">
+                        <div >
+                            <label class="marginstreet">11/12/2022</label>
+                            <label class="streetsize">c/NameStr 291, BCN</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="margegran">
+                    <img src="https://www.marquid.com/wp-content/uploads/2017/06/6197706_orig.jpg" width="75" height="75 ">
+                    <div>
+                        <div class="aaa">
+                            <label class="marginnameofevent">EventName</label>
+                            <img class="margefotoperfil" src="https://cdn.pixabay.com/photo/2021/05/04/13/29/portrait-6228705_960_720.jpg" width="45" height="45"/>
+                            <div class="marge4">
+                                <label class="nameofcreator">CreatorName</label>
+                                <form>
+                                    <div class="clasificacion">
+                                        <input id="radio1" type="radio" name="estrellas" value="5">
+                                        <label for="radio1">★</label>
+                                        <input id="radio2" type="radio" name="estrellas" value="4">
+                                        <label for="radio2">★</label>
+                                        <input id="radio3" type="radio" name="estrellas" value="3">
+                                        <label for="radio3">★</label>
+                                        <input id="radio4" type="radio" name="estrellas" value="2">
+                                        <label for="radio4">★</label>
+                                        <input id="radio5" type="radio" name="estrellas" value="1">
+                                        <label for="radio5">★</label>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div >
+                            <label class="marginstreet">11/12/2022</label>
+                            <label class="streetsize">c/NameStr 291, BCN</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="margegran">
+                    <img src="https://www.marquid.com/wp-content/uploads/2017/06/6197706_orig.jpg" width="75" height="75 ">
+                    <div>
+                        <div class="aaa">
+                            <label class="marginnameofevent">EventName</label>
+                            <img class="margefotoperfil" src="https://cdn.pixabay.com/photo/2021/05/04/13/29/portrait-6228705_960_720.jpg" width="45" height="45"/>
+                            <div class="marge4">
+                                <label class="nameofcreator">CreatorName</label>
+                                <form>
+                                    <div class="clasificacion">
+                                        <input id="radio1" type="radio" name="estrellas" value="5">
+                                        <label for="radio1">★</label>
+                                        <input id="radio2" type="radio" name="estrellas" value="4">
+                                        <label for="radio2">★</label>
+                                        <input id="radio3" type="radio" name="estrellas" value="3">
+                                        <label for="radio3">★</label>
+                                        <input id="radio4" type="radio" name="estrellas" value="2">
+                                        <label for="radio4">★</label>
+                                        <input id="radio5" type="radio" name="estrellas" value="1">
+                                        <label for="radio5">★</label>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div >
                             <label class="marginstreet">11/12/2022</label>
                             <label class="streetsize">c/NameStr 291, BCN</label>
                         </div>
@@ -108,6 +136,35 @@
 
 
 <style>
+#form {
+  width: 250px;
+  margin: 0 auto;
+  height: 50px;
+}
+
+#form div {
+  text-align: center;
+}
+
+#form label {
+  font-size: 20px;
+}
+
+input[type="radio"] {
+  display: none;
+}
+
+label {
+  color: grey;
+}
+
+.clasificacion {
+  direction: rtl;
+  unicode-bidi: bidi-override;
+}
+
+
+
 
 
 .centerall{
@@ -177,9 +234,6 @@
     margin-left: 10px;
 }
 
-.margetop3{
-    margin-top: 5px;
-}
 
 .margefotoperfil{
     margin-top: 5px;
