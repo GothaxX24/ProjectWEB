99da98de75b3473bd89eb7a21a760b5c512e7398
tarekDiaz <tarek.diaz@students.salle.url.edu>
2022-11-12 18:31:12 +0100

    xd

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,3 +1,17 @@
 <template>
-    
+    <div class="menu">
+        <button class="btn-menu">Menu</button>
+        <div class="menulinks">
+            <a href="#">Profile</a>
+            <a href="#">Friends</a>
+            <a href="#">Search event</a>
+            <a href="#">Create event</a>
+        </div>
+    </div>
 </template>
+
+<style>
+    .btn-menu{
+        background-color: rgb(38, 11, 214);
+    }
+</style>
