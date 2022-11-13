093f42ffec8d30f3303fb0a9be35d4e03bd1840f
mireia11 <115173193+mireia11@users.noreply.github.com>
2022-11-13 02:15:27 +0100

    chat

--- src/views/Chats.vue
+++ src/views/Chats.vue
@@ -1,22 +1,22 @@
 <template >
         
-    <header class ="header_friends">
+    <header class ="header_chats">
         
         <h1>Chats</h1>
         
     </header>
         
-    <div> 
+    <div class = chats_body> 
               
-            <div class = body_chats1>
+            <div class = chats_text>
                 <label><strong>Your friends: </strong></label>         
             </div>
 
-            <div class = body_chats1>
+            <div class = chats_text>
                 <img class="circular-image" src="https://2.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=180" width="30" height="30"/>
                 <label>User 1</label> 
-                <div class = body_chats10>
-                    <button class="Chats-button"><strong>Send message</strong></button>
+                <div class = chats_send>
+                    <button class= "chats_send" @click="$router.push('/inChat')" type="button"><strong>Send message</strong></button>
                 </div>
                 <div class = body_chats11>
                     <label><strong>You have 1 message!</strong></label>
@@ -24,27 +24,27 @@
             </div>
 
     
-            <div class = body_chats1>
+            <div class = chats_text>
                 <img class="circular-image" src="https://2.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=180" width="30" height="30"/>
                 <label>User 2</label>
-                <div class = body_chats10>
-                    <button class="Chats-button"><strong>Send message</strong></button> 
+                <div class = chats_send>
+                    <button class= "chats_send" @click="$router.push('/inChat')" type="button"><strong>Send message</strong></button>
                 </div>
             </div>
             
-            <div class = body_chats1>
+            <div class = chats_text>
                 <img class="circular-image" src="https://2.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=180" width="30" height="30"/>
                 <label>User 3</label> 
-                <div class = body_chats10>
-                <button class="Chats-button"><strong>Send message</strong></button>
+                <div class = chats_send>
+                    <button class= "chats_send" @click="$router.push('/inChat')" type="button"><strong>Send message</strong></button>
                 </div>
             </div>
            
-            <div class = body_chats1>
+            <div class = chats_text>
                 <img class="circular-image" src="https://2.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=180" width="30" height="30"/>
                 <label>User 4</label> 
-                <div class = body_chats10>
-                    <button class="Chats-button"><strong>Send message</strong></button>
+                <div class = chats_send>
+                    <button class= "chats_send" @click="$router.push('/inChat')" type="button"><strong>Send message</strong></button>
                 </div>
                 <div class = body_chats11>
                     <label><strong>You have 1 message!</strong></label>
@@ -53,54 +53,68 @@
 
             <br>
         
-            <div class = body_chats1>
+            <div class = chats_text>
                 <label><strong>Other suggestions:</strong></label> 
             </div>
            
-            <div class = body_chats1>
+            <div class = chats_text>
                 <img class="circular-image" src="https://2.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=180" width="30" height="30"/>
                 <label>User 5</label> 
-                <div class = body_chats10>
-                    <button class="Chats-button"><strong>Send message</strong></button>
+                <div class = chats_send>
+                    <button class= "chats_send" @click="$router.push('/inChat')" type="button"><strong>Send message</strong></button>
                 </div>
             </div>
             
-            <div class = body_chats1>
+            <div class = chats_text>
                 <img class="circular-image" src="https://2.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=180" width="30" height="30"/>
                 <label>User 6</label>  
-                <div class = body_chats10>
-                    <button class="Chats-button"><strong>Send message</strong></button>
+                <div class = chats_send>
+                    <button class= "chats_send" @click="$router.push('/inChat')" type="button"><strong>Send message</strong></button>
                 </div>  
             </div>
 
-            <div class = body_chats1>
+            <div class = chats_text>
                 <img class="circular-image" src="https://2.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=180" width="30" height="30"/>
                 <label>User 7</label>  
-                <div class = body_chats10>
-                    <button class="Chats-button"><strong>Send message</strong></button>
+                <div class = chats_send>
+                    <button class= "chats_send" @click="$router.push('/inChat')" type="button"><strong>Send message</strong></button>
                 </div>  
             </div>
     </div>      
     
 </template>
 <style>
-.header_friends{ 
+.header_chats{ 
 
 font-family: 'Courier New', Courier, monospace;
 font-size:medium;   
 }
 
-.body_chats1{
+.chats_body{
+background-color: white;
+display: flex;
+justify-content: center;
+flex-direction: column;
+align-items: center;
+border: none;
+max-width: 100%;
+
+}
+
+.chats_text{
 border:3px solid black;
+margin-bottom: 1px;
 font-size: "2";
 background-color: turquoise;
 font-family : Arial, Helvetica, sans-serif;
 display: flex;
 flex-direction: row;
+width: 100%;
+max-width: 600px;
 }
 
 
-.body_chats10{
+.chats_send{
     
     font-size: "2";
     color: rgb(255, 255, 255);
@@ -111,10 +125,12 @@ flex-direction: row;
     text-align: center;
     padding: 0 50px 0 55px;
     margin-top: 3px;
+    margin-bottom: 3px;
+    border-radius: 15px;
 }
 
 .body_chats11 {
-    border: 5px solid rgb(255, 255, 255);
+    border: 3px solid rgb(255, 255, 255);
     font-size: "2";
     color: rgb(0, 0, 0);
 
@@ -122,8 +138,6 @@ flex-direction: row;
     flex-direction:row;
     text-align:right;
     padding: 1 50px 1 60px;
-    margin-top: 0px;
-
 }
 </style>
 
