462fb159cd87630109e2881d1636930ae3613cc2
tarekDiaz <tarek.diaz@students.salle.url.edu>
2023-01-20 19:12:22 +0100

    Revert "header menu components"
    This reverts commit 569de86b265250c138edb446a2951cc44a21860a.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
-import { createApp, VueElement } from 'vue'
+import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from "./router/index.js";
-import userlog from "./singleton/userlog";
-
-VueElement.prototype.$userlog = userlog;
 
 createApp(App).use(router).mount('#app')
