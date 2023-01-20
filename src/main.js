569de86b265250c138edb446a2951cc44a21860a
tarekDiaz <tarek.diaz@students.salle.url.edu>
2023-01-20 19:03:55 +0100

    header menu components

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import { createApp } from 'vue'
+import { createApp, VueElement } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from "./router/index.js";
+import userlog from "./singleton/userlog";
+
+VueElement.prototype.$userlog = userlog;
 
 createApp(App).use(router).mount('#app')
--- src/views/Friends.vue
+++ src/views/Friends.vue
@@ -94,37 +94,21 @@
     }
 </script>
 
+<script setup>
+    import HeaderMenu from '../components/HeaderMenu.vue'
+</script>
+
 <template >
     <head>
         <link rel="stylesheet" href="style.css" />
     </head>
-    <div class="menu">
-        <img class="icono-menu" src="https://cdn.pixabay.com/photo/2021/05/04/13/29/portrait-6228705_960_720.jpg"/>
-        <div class="menulinks">
-            <RouterLink to="/profile">
-                <img class="iconos-menu" src="https://static.thenounproject.com/png/638636-200.png">
-                <a>Profile </a>      
-            </RouterLink>
-            <RouterLink to="/friends">
-                <img class="iconos-menu" src="https://cdn-icons-png.flaticon.com/512/880/880594.png">
-                <a>Friends</a>
-            </RouterLink>
-            <RouterLink to="/eventslist">
-                <img class="iconos-menu" src="https://uxwing.com/wp-content/themes/uxwing/download/user-interface/search-icon.png">
-                <a>Search event</a>
-            </RouterLink>    
-            <RouterLink to="/createevent">
-                <img class="iconos-menu" src="https://cdn-icons-png.flaticon.com/512/32/32339.png">
-                <a>Create event</a>
-            </RouterLink> 
-            
-        </div>
-    </div>
-            <header class ="header_friends">
+    <HeaderMenu />
+
+    <header class ="header_friends">
                 
-                <h1>Friends</h1>
+        <h1>Friends</h1>
                 
-            </header>
+    </header>
                 
             <div class = all> 
                       
