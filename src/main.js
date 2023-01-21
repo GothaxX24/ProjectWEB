40c71799131f3be0c4c23ba33bb296b4c3085ad0
tarekDiaz <tarek.diaz@students.salle.url.edu>
2023-01-21 14:12:59 +0100

    homepage elim

--- src/main.js
+++ src/main.js
@@ -4,20 +4,5 @@ import App from './App.vue'
 import router from "./router/index.js";
 
 //import UserLog from './singletone/userlog.js'
-/*
-export default {
-    name: 'app',
-    created() {
-        this.$on('submit', (data) => {
-            this.userLog = new UserLog(data.id, data.token)
-        })
-    },
-    provide () {
-        return {
-            userLog: this.userLog
-        }
-    }
-}
-*/
 
 createApp(App).use(router).mount('#app')
